eaaeefe7e99ffe6c29492fc43add4dc98b710671
Subritt <subrittburlakoti@gmail.com>
2020-11-03 00:19:34 +0545

    created about page and path

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# home view
+# home page
 def home(request):
     return HttpResponse('<h1>Blog Home</h1>')
 
+# about page
+def about(request):
+    return HttpResponse('<h1>Blog About</h1>')
