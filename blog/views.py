f30eeed48a1346f6eb50998c705ba690cea90298
Subritt <subrittburlakoti@gmail.com>
2020-11-03 00:12:30 +0545

    created a function to handle home view inside blog's views.py and created urls.py file and setup the path for blog-home

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# home view
+def home(request):
+    return HttpResponse('<h1>Blog Home</h1>')
+
