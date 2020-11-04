ec2fab28e5f760a5264be6f3d11ae0bbd5998349
Subritt <subrittburlakoti@gmail.com>
2020-11-04 23:08:05 +0545

    created a dummy data and accessed it from template using code blocks

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
 
 # home page
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/home.html', context)
 
 # about page
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', { 'title': 'About'})
