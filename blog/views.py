bd3289ca3c2b2a9805f179437f51d0d6bd12c9e8
Subritt <subrittburlakoti@gmail.com>
2020-11-04 22:47:23 +0545

    initialied templates

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 
 # home page
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return render(request, 'blog/home.html')
 
 # about page
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
