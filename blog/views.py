4b5c95e47fd439ef6d279d7ca75e6cc954205946
Subritt <subrittburlakoti@gmail.com>
2020-11-08 19:34:13 +0545

    using class-based views. created a PostListView class for home view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # home page
@@ -7,6 +8,12 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # <app>/<post>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
 # about page
 def about(request):
