64ed42366b04cdb8aeea84835870abd095cc5cf0
Subritt <subrittburlakoti@gmail.com>
2020-11-12 23:17:32 +0545

    add a password-reset-complete route

--- BlogApp/urls.py
+++ BlogApp/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     path('', include('blog.urls')),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
